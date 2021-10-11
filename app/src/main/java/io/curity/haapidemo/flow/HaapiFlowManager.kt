faeed5cf3a1cdd851a2499ff1b5ed8867e54dcba
Henri La <henri.la@curity.io>
2021-10-11 16:22:20 +0200

    HSA-71: Fixes lint warnings/errors

--- app/src/main/java/io/curity/haapidemo/flow/HaapiFlowManager.kt
+++ app/src/main/java/io/curity/haapidemo/flow/HaapiFlowManager.kt
@@ -16,6 +16,7 @@
 
 package io.curity.haapidemo.flow
 
+import android.annotation.SuppressLint
 import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
@@ -383,10 +384,12 @@ private fun HaapiFlowConfiguration.tokenFormBodyBuilder(grantType: String): Form
 //region Private extensions for disabling SSL Verification !!!
 private val trustManager = object : X509TrustManager
 {
+    @SuppressLint("TrustAllX509TrustManager")
     override fun checkClientTrusted(p0: Array<out X509Certificate>, p1: String)
     {
     }
 
+    @SuppressLint("TrustAllX509TrustManager")
     override fun checkServerTrusted(p0: Array<out X509Certificate>, p1: String)
     {
     }
